c7ae56506f2c3bb04340ba501726cbe599a502ac
Filip Chabik <filip.chabik@erasys.de>
2016-11-18 13:38:43 +0100

    Allow to collect more than one mysql instance per host
    (Currently requires a copy/link of this module per instance)

--- mysql.py
+++ mysql.py
@@ -13,6 +13,7 @@
 #   Port 3306 (optional)
 #   User root
 #   Password xxxx
+#   Instance xxxx
 #   HeartbeatTable percona.heartbeat (optional, if using pt-heartbeat)
 #   Verbose true (optional, to enable debugging)
 #  </Module>
@@ -21,6 +22,7 @@
 #
 # Author: Chris Boulton <chris@chrisboulton.com>
 # Added metrics for Grafana Dashboards by Matej Zerovnik <matej@zunaj.si>
+# Added support for multiple instances by Filip Chabik <filip.chabik@erasys.de>
 # License: MIT (http://www.opensource.org/licenses/mit-license.php)
 #
 
@@ -43,6 +45,7 @@ MYSQL_CONFIG = {
 	'Password':       '',
 	'HeartbeatTable': '',
 	'Verbose':        False,
+	'Instance':       '',
 }
 
 MYSQL_STATUS_VARS = {
@@ -493,14 +496,18 @@ def dispatch_value(prefix, key, value, type, type_instance=None):
 	log_verbose('Sending value: %s/%s=%s' % (prefix, type_instance, value))
 	if value is None:
 		return
-	value = int(value) # safety check
 
-	if COLLECTD_ENABLED:
-		val               = collectd.Values(plugin='mysql', plugin_instance=prefix)
-		val.type          = type
-		val.type_instance = type_instance
-		val.values        = [value]
-		val.dispatch()
+	if not COLLECTD_ENABLED:
+		return
+
+	value = int(value) # safety check
+	val					= collectd.Values(plugin='mysql', plugin_instance=prefix)
+	val.plugin			= 'mysql.%s' % MYSQL_CONFIG['Instance']
+	val.plugin_instance	= prefix
+	val.type			= type
+	val.type_instance	= type_instance
+	val.values			= [value]
+	val.dispatch()
 
 def configure_callback(conf):
 	global MYSQL_CONFIG
