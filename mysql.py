d18c4c0ee3c25617938fe98981810774b5edbd41
Adrien SEVERE <asevere@commentcamarche.net>
2021-05-20 15:57:34 +0200

    Mariadb 10.5 (possibly all version since 10.3.23 ?) expose 'Innodb_max_trx_id' without 'Innodb_purge_trx_id' resulting in "Unhandled python exception in read callback: KeyError: 'Innodb_purge_trx_id'"

--- mysql.py
+++ mysql.py
@@ -349,7 +349,7 @@ def fetch_mysql_status(conn):
 		status[row['Variable_name']] = row['Value']
 
 	# calculate the number of unpurged txns from existing variables
-	if 'Innodb_max_trx_id' in status:
+	if 'Innodb_max_trx_id' in status and 'Innodb_purge_trx_id' in status:
 		status['Innodb_unpurged_txns'] = int(status['Innodb_max_trx_id']) - int(status['Innodb_purge_trx_id'])
 
 	if 'Innodb_lsn_last_checkpoint' in status:
